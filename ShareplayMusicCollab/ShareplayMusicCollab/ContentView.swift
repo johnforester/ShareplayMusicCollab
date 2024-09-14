67fab1bb21482c638fbab55f0806907fcdd5fc42
John Forester <forester.john@gmail.com>
2024-09-14 19:02:20 -0400

    fix issue with multiple selection windows

--- ShareplayMusicCollab/ShareplayMusicCollab/ContentView.swift
+++ ShareplayMusicCollab/ShareplayMusicCollab/ContentView.swift
@@ -12,20 +12,12 @@ import RealityKitContent
 #if os(visionOS)
 struct ContentView: View {
     @Environment(AppModel.self) private var appModel
-    @State private var instrumentSelected = false  // Track if the instrument is selected
-
+    
     var body: some View {
         VStack {
-            if instrumentSelected {
-                // Show MIDI Monitor View after instrument is selected
-                MIDIMonitorView()
-                    .environment(appModel)
-            } else {
-                // Show Instrument Selection first, and update state when an instrument is selected
-                InstrumentSelectAndShareplayView(onInstrumentSelected: {
-                    instrumentSelected = true  // Update state when instrument is selected
-                })
-            }
+            // Show MIDI Monitor View after instrument is selected
+            MIDIMonitorView()
+                .environment(appModel)
             //ToggleImmersiveSpaceButton()
         }
         .padding()
